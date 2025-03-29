2d17fe1438eda38b2174032ee6ad4e6cf5ad3859
priyarvth <priyarvth19@gmail.com>
2025-03-29 22:34:43 +0530

    Add files via upload
    assignment2

---
+++ Chrome.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		float version=91.1f;
+		System.out.println(version);
+		
+		String developer="Google";
+		System.out.println(developer);
+		
+		boolean beta=false;
+		System.out.println(beta);
+		
+		int releaseYear=2008;
+		System.out.println(releaseYear);
+		
+		char shortcutKey='C';
+		System.out.println(shortcutKey);
+
+	}
+
+}
